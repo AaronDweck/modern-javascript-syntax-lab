0838ce554febde9f965ffde303bacc556c9cb564
Aaron Dweck <aarondweck24@gmail.com>
2025-01-27 10:28:38 +0000

    finished all exercises

--- app.js
+++ app.js
@@ -47,11 +47,11 @@ console.log(secondIngredient)
 const car = {
     make: 'Audi',
     model: 'Q5',
-  };
-  
-  // Your code here:
-  
-const {make, model} = car
+};
+
+// Your code here:
+
+const { make, model } = car
 
 console.log(make)
 console.log(model)
@@ -86,14 +86,130 @@ console.log(uncontroversialPizzaToppings)
 const anotherCar = {
     make: 'Toyota',
     model: 'RAV4',
-  };
-  
-  // Your code here:
-  
-const myCar = {...anotherCar}  
+};
+
+// Your code here:
+
+const myCar = { ...anotherCar }
 
 myCar.make = 'Honda'
 myCar.model = 'Odyssey'
 
 console.log(anotherCar)
 console.log(myCar)
+
+// ! Exercise 6:
+// a. Define a variable named `propertyName` and assign a string (like 
+//    'username', 'age', or 'email') to it.
+// 
+// b. Create an object named `userProfile`. 
+// 
+// c. Use `propertyName` as a dynamic key in `userProfile`. Assign it a 
+//    relevant value.
+//
+// d. Console log the `userProfile` object to see the result.
+//
+// Your code here:
+
+const propertyName = 'username'
+
+const userProfile = {}
+
+userProfile[propertyName] = 'hello'
+
+console.log(userProfile)
+
+// ! Exercise 8:
+// a. Create a function with two parameters, `noun` and `adjective`.
+// 
+// b. Give `noun` a default value of "cat" and `adjective` a default value of 
+//    "orange".
+//
+// c. The function should log a sentence 'The cat is orange.' by default, but 
+//    should substitute the appropriate parameters when it is supplied with 
+//    arguments.
+//
+// Your code here:
+
+function logSentence(noun = 'cat', adjective = 'orange') {
+    console.log(`the ${noun} is ${adjective}`)
+}
+
+logSentence('bike')
+
+// ! Exercise 9:
+// a. Convert the following `if...else` statement into a ternary:
+//
+//    if (pizza === 'tasty') {
+//      console.log('yum');
+//    } else {
+//      console.log('yuck');
+//    }
+//
+// Starting code (don't modify this):
+
+const pizza = 'taty';
+
+// Your code here:
+
+pizza === 'tasty' ? console.log('yum') : console.log('yuck')
+
+// ! Exercise 10:
+// ! 10.1: Set language
+// a. Construct a single line of code that assigns a default value using the 
+//    logical OR operator. This line should match the logic of the following 
+//    statement:
+//
+//    "lang is equal to localLangConfig or the default value of English."
+//
+// b. Create a variable called `lang`.
+//
+// c. Assign `lang` the value of localLangConfig or 'en' as a default if 
+//    `localLangConfig is falsy.
+//
+// d. Log the value of `lang` to the console.
+//
+// Your code here (localLangConfig is provided to get you started):
+
+// Simulated language configuration (change this variable to test)
+const localLangConfig = 'fr'; // Change to 'es', 'fr', etc., or leave it `null`.
+
+const lang = localLangConfig || 'en'
+
+console.log(lang)
+
+// ! 10.2: Set website theme
+// Intro: In this exercise, you'll construct a single line of code that assigns 
+//        a default value to a variable named `theme` using the logical OR 
+//        operator. This line should match the logic of the following statement:
+//
+//        "theme is equal to savedUserTheme or the default value of light."
+//
+// a. Create a variable called `theme`.
+//
+// b. Assign `theme` the value of `savedUserTheme` or 'light' as a default.
+//
+// c. Log the value of `theme` to the console.
+//
+// Your code here (`savedUserTheme` is provided to get you started):
+
+// Simulated user theme preference (change this variable to test)
+const savedUserTheme = 'dark'; // Change to 'dark', etc., or leave it `null`.
+
+const theme = savedUserTheme || 'light'
+
+console.log(theme)
+
+// ! Exercise 11:
+// a. Use optional chaining in a console.log so that a console log of
+//    `adventurer.cat.age` returns `undefined` instead of an error.
+//
+// Starting code (don't modify this):
+
+const adventurer = {
+    name: 'Alice',
+  };
+  
+  // Your code here:
+  
+console.log(adventurer.cat?.name)  
